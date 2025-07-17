9d198f39fd317a1492d485c2fe2c8221f2aa8187
Yubesh <yubeshbhattarai@gmail.com>
2025-07-17 15:38:40 +0545

    Finally fixed the mobile navbar

--- src/Components/Mobile.tsx
+++ src/Components/Mobile.tsx
@@ -3,25 +3,36 @@
 import React,{useState} from 'react';
 import Link from "next/link";
 
+const mobile =[
+  {id: 1, name: 'home ' , url: "/"},
+  {id: 2, name: 'about ' , url: "/about"},
+  {id: 3, name: 'skills ' , url: "/skills"},
+  {id: 4, name: 'projects ' , url: "/projects"},
+  {id: 5, name: 'contacts ' , url: "/contacts"}
+]
+
+
 export default function Mobile(){
   const[open,setOpen]=useState(false);
 
     
     return(
 
-      <div className="  md:hidden flex flex-col items-center justify-center top-4 right-12 z-10  absolute text-xl w-44    ">
-           <button onClick={() => setOpen(!open)}>{open ? "off" : "On"}</button>
-             <div className={` bg-white/75 bg-blur-[2px] py-4 flex flex-col justify-center items-center w-40 h-[18rem] gap-5 text-2xl  ${open ? "block" : "hidden"}`}>
-               <Link href="/">Home</Link>
-               <Link href="/about">about </Link>
-               <Link href="/skills">skills</Link>
-               <Link href="/projects">projects</Link>
-               <Link href="/contacts">contacts</Link>
+      <div className="w-40  md:hidden flex flex-col items-center justify-center top-1 right-12 z-50  absolute text-xl   backdrop-blur-[2px] mt-4  rounded-md   ">
+          {open ? <span onClick={()=>setOpen(false)}>X</span> : <span onClick={()=>setOpen(true)}>≡</span>}
+        
+      
+           {open &&   mobile.map((item)=>(
+             <div key={item.id } className={` bg-white/75 bg-blur-[2px] py-4 flex flex-col justify-center items-center w-60 h-[5rem] gap-5 text-2xl  `}  onClick={()=>setOpen(false)} >  
+           <Link  href={item.url}>{item.name}</Link>
            </div>
-       </div>
+         ))}         
+             </div>
+          
+      
     )
 }
            
-           
+      
      
     
